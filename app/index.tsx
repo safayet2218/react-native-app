289c1736986c3d0b8429da184bdc006174f8bccb
safayte2218 <safayet2218@gmail.com>
2024-09-27 22:41:28 +0600

    assets and constant setup

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ import { Link } from 'expo-router'
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-3xl'>index</Text>
+      <Text className='text-3xl font-pblack'>index</Text>
       <StatusBar style='auto'/>
       <Link href="/profile" style={{color:'blue'}}>
       Go To profile
